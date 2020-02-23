3d7d7f402c4c4efa433fd5f3d0df2f06ae060831
Hossein Rimaz <mhrimaz@yahoo.com>
2020-02-23 20:19:54 +0330

    update base functionality

--- src/parser/base.py
+++ src/parser/base.py
@@ -2,32 +2,41 @@ from tqdm import tqdm_notebook as tqdm
 import pandas as pd
 
 
-def correct_types(df, columns):
-    for col in df.columns:
-        type_ = columns.get(col)
-        if type_ is not None:
-            df[col] = df[col].astype(type_)
-
-
 def dump_to_db(con, table_name, df):
     con.put(key=table_name, value=df, format='table', data_coulmns=True, append=True,
             min_itemsize=500)
 
-
-def parse_jsons(l_json, con, threshold, subject):
-    dfs = []
+def parse_jsons_as_df(list_of_json_paths,subject):
     package = f'src.parser.{subject}_parser'
     parse_one_json = getattr(__import__(package, fromlist=['parse_one_json']), 'parse_one_json')
-    for i, json_path in tqdm(enumerate(l_json), total=len(l_json)):
+    dataframes = []
+    for i, json_path in tqdm(enumerate(list_of_json_paths), total=len(list_of_json_paths)):
+        dataframes.append(parse_one_json(json_path))
+        
+        
+    return pd.concat(dataframes, ignore_index=True)
+
+
+
+def parse_jsons(list_of_json_paths, con, subject):
+    dataframes_chunk = []
+    chunk_size = 100
+    
+    package = f'src.parser.{subject}_parser'
+    parse_one_json = getattr(__import__(package, fromlist=['parse_one_json']), 'parse_one_json')
+    
+    for i, json_path in tqdm(enumerate(list_of_json_paths), total=len(list_of_json_paths)):
         if subject not in json_path:
             continue
-
-        dfs.append(parse_one_json(json_path, threshold))
-        if len(dfs) > 99:
-
-            df = pd.concat(dfs, ignore_index=True)
+            
+        dataframes_chunk.append(parse_one_json(json_path))
+        
+        if len(dataframes_chunk) > chunk_size:
+            df = pd.concat(dataframes_chunk, ignore_index=True)
             dump_to_db(con, table_name=subject, df=df)
-            dfs = []
-    if len(dfs) > 0:
-        df = pd.concat(dfs, ignore_index=True)
+            dataframes_chunk = []
+            
+    # concat remaining items in the chunk to the dataframes holder
+    if len(dataframes_chunk) > 0:
+        df = pd.concat(dataframes_chunk, ignore_index=True)
         dump_to_db(con, table_name=subject, df=df)
